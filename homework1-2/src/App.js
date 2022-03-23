2440be11d9687812427b3a84158f296d18ab2fab
shodik setiawan <shodiksetiawan14@gmail.com>
2022-03-23 13:49:42 +0700

    last commit

---
+++ homework1-2/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import data from './sample.js';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <img src={data.album.images[0].url} width="300" height="300" className="rotate" />
+      </div>
+      <div>
+        <h2>{data.name}</h2>
+        <h3>{data.album.name}</h3>
+        <p>{data.artists[0].name}</p>
+	<button class="submit"><a href={data.album.artists[0].external_urls.spotify}>select</a></button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
